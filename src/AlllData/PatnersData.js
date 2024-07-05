1163199bcc43f0e981c98bf28b0b0ec423bec997
5ujay <sujaypagam777@gmail.com>
2024-07-05 12:04:31 +0530

    test

---
+++ src/AlllData/PatnersData.js
@@ -0,0 +1,54 @@
+import patner1 from "../assets/PatnersImg/patner1.jpg";
+import patner2 from "../assets/PatnersImg/patner2.jpg";
+import patner3 from "../assets/PatnersImg/patner3.jpg";
+import patner4 from "../assets/PatnersImg/patner4.jpg";
+import patner5 from "../assets/PatnersImg/patner5.jpg";
+import patner6 from "../assets/PatnersImg/patner6.jpg";
+import patner7 from "../assets/PatnersImg/patner7.jpg";
+
+const PaternsData = [
+  {
+    id: 0,
+    image: patner1,
+    name: "Name of patner",
+    date: "Date",
+  },
+  {
+    id: 1,
+    image: patner2,
+    name: "Name of patner",
+    date: "Date",
+  },
+  {
+    id: 2,
+    image: patner3,
+    name: "Name of patner",
+    date: "Date",
+  },
+  {
+    id: 3,
+    image: patner4,
+    name: "Name of patner",
+    date: "Date",
+  },
+  {
+    id: 4,
+    image: patner5,
+    name: "Name of patner",
+    date: "Date",
+  },
+  {
+    id: 5,
+    image: patner6,
+    name: "Name of patner",
+    date: "Date",
+  },
+  {
+    id: 6,
+    image: patner7,
+    name: "Name of patner",
+    date: "Date",
+  },
+];
+
+export default PaternsData;
---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>BGMI Clone</h1>
+    </>
+  );
+}
+
+export default App;
